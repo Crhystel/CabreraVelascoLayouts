d9ec5bd213b5fed18ce5813377771d02d51ce29c
Crhystel <Crhystel@DESKTOP-IJ6GBVS>
2024-11-15 07:51:38 -0500

    Agregar archivos de proyecto.

---
+++ CabreraVelascoLayouts/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CabreraVelascoLayouts
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CabreraVelascoLayouts/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CabreraVelascoLayouts
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
